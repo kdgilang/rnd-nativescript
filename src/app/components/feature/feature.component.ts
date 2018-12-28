08ba7c76d4c02291dffc42c45cff1ba5133bf852
FLY\Yopie <gusti_yoga@ymail.com>
2018-12-28 22:57:14 +0800

    add elevation and banner title

--- src/app/components/feature/feature.component.ts
+++ src/app/components/feature/feature.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { registerElement } from 'nativescript-angular/element-registry';
+import { CardView } from 'nativescript-cardview';
 
+registerElement('CardView', () => CardView);
 @Component({
   selector: 'app-feature',
   templateUrl: './feature.component.html',
