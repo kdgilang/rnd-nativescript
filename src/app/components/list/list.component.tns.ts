c06f43e3707d963cea057c3667583c9d7bf7c509
Barayuda Gautama <saya.bara@gmail.com>
2018-12-30 01:03:12 +0800

    Styling listview

---
+++ src/app/components/list/list.component.tns.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+// temp service
+class Post {
+  constructor(public name: string) { }
+}
+
+let orderCarte = ["Paket Mantap", "Chicken Big Burger", "Spicy Chicken Original"];
+
+// end temp service
+  
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListComponent implements OnInit {
+
+  public orders: Array<Post>;
+
+  constructor() { }
+
+  public onItemTap(args) {
+      console.log("Item Tapped at cell index: " + args.index);
+  }
+  
+  ngOnInit() {
+    this.orders = [];
+
+    for (let i = 0; i < orderCarte.length; i++) {
+        this.orders.push(new Post(orderCarte[i]));
+    }
+  }
+
+}
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
 
